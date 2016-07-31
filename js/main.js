58f7f2a30be2ac1f15c9047b9607ac66b503a3b1
Danilo <danilonewtrue@gmail.com>
2016-07-31 14:58:41 -0300

    Tipos Basicos

--- js/main.js
+++ js/main.js
@@ -9,10 +9,62 @@ var Car = (function () {
     return Car;
 }());
 var getCar = function (car) {
-    console.log(car.brand, car.name);
+    //console.log(car.brand, car.name);
 };
 var Camaro = new Car();
 Camaro.brand = "Chevrolet";
 Camaro.name = "Camaro";
 Camaro.power = 45;
 getCar(Camaro);
+/*Usando tipos do Typescript */
+// Criando uma variavel do tipo Boolean
+var test; // aceita apenas os valores: true ou false
+var id = 2; //aceita valores numericos
+var decimal = 5.4; // aceita valores decimais
+var hex = 0xf00d; // aceita valor hexadecimais
+var bin = 10; // aceita valores binarios
+var octal = 484; // aceita valores octadecimal
+var name = "Danilo";
+var carro = 'Camaro';
+// Vamos criar uma variavel que pega todos os valores e salva como String
+var text = " <h1> name: " + name + " Carro: " + carro + " </h1>";
+// Adicionando a variavel 'text' para o nosso HTML
+document.body.innerHTML = text;
+/* Vamos ver como funciona os Arrays */
+/* Em Typescript podemos criar Array Generico */
+var fruits = ["Apple", "Watermellon", "lemon"];
+console.log(fruits);
+/* Em Typescript podemos criar Array Generico */
+var fruits = ["Apple", "Watermellon", "lemon"];
+console.log(fruits);
+/*Trabalhando com 'enum' - Variaveis com varios tipos de estados */
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+;
+console.log(Color.blue); //Para imprimir o valor de um 'enum' podemos usar essa forma
+/* Caso quisermos mudar a ordem do enum podemos fazer assim: */
+var Color2;
+(function (Color2) {
+    Color2[Color2["red"] = 1] = "red";
+    Color2[Color2["green"] = 2] = "green";
+    Color2[Color2["blue"] = 3] = "blue";
+})(Color2 || (Color2 = {}));
+;
+console.log(Color.blue);
+/* E assim ele muda a ordem começando pelo numero 1 */
+//Para pegar o valor String do Enum
+console.log(Color2[2]);
+/* Variavel any - pode receber qualquer tipo de valor */
+var list = 23;
+console.log(list);
+list = "Minha string ....";
+console.log(list);
+/* Função VOID */
+function printNumber(num) {
+    alert(num);
+    //Função sem retorno
+}
--- js/main.ts
+++ js/main.ts
@@ -19,7 +19,7 @@ class Car {
 }
 
 var getCar = function(car:iCar) {
-    console.log(car.brand, car.name);
+    //console.log(car.brand, car.name);
 };
 
 var Camaro = new Car();
@@ -28,3 +28,63 @@ Camaro.name = "Camaro";
 Camaro.power = 45;
 
 getCar(Camaro);
+
+
+/*Usando tipos do Typescript */
+
+// Criando uma variavel do tipo Boolean
+var test:boolean; // aceita apenas os valores: true ou false
+var id:number = 2; //aceita valores numericos
+
+var decimal:number = 5.4; // aceita valores decimais
+var hex:number = 0xf00d; // aceita valor hexadecimais
+var bin:number = 0b1010; // aceita valores binarios
+var octal:number = 0o744; // aceita valores octadecimal
+
+var name:string = "Danilo";
+var carro:string = 'Camaro';
+
+// Vamos criar uma variavel que pega todos os valores e salva como String
+
+var text = ` <h1> name: ${name} Carro: ${carro} </h1>`;
+
+// Adicionando a variavel 'text' para o nosso HTML
+document.body.innerHTML = text;
+
+/* Vamos ver como funciona os Arrays */
+
+/* Em Typescript podemos criar Array Generico */
+
+var fruits:string[] = ["Apple","Watermellon", "lemon"];
+console.log(fruits);
+
+/* Em Typescript podemos criar Array Generico */
+var fruits:Array<string> = ["Apple","Watermellon", "lemon"];
+console.log(fruits);
+
+/*Trabalhando com 'enum' - Variaveis com varios tipos de estados */
+
+enum Color {red, green, blue};
+console.log(Color.blue); //Para imprimir o valor de um 'enum' podemos usar essa forma
+
+/* Caso quisermos mudar a ordem do enum podemos fazer assim: */
+enum Color2 {red = 1, green, blue};
+console.log(Color.blue);
+/* E assim ele muda a ordem começando pelo numero 1 */
+
+//Para pegar o valor String do Enum
+console.log(Color2[2]);
+
+/* Variavel any - pode receber qualquer tipo de valor */
+var list:any = 23;
+console.log(list);
+list = "Minha string ....";
+console.log(list);
+
+/* Função VOID */
+
+function printNumber(num:number):void {
+    alert(num);
+    //Função sem retorno
+}
+
